69e0b2ab183a9e083cf6655204e1d7a36d2d17c8
Urszula-Molska <105520935+Urszula-Molska@users.noreply.github.com>
2022-10-12 17:35:46 +0000

    Deploying to gh-pages from @ Urszula-Molska/goit-js-hw-09@11ec8b2e42a1f20753855e8d2d1581e7a197d78b 🚀

---
+++ 01-color-switcher.6a10455a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){timerId=setInterval((function(){"#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.6a10455a.js.map
