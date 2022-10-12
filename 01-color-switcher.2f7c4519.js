72991d1dbec0458a81d8442a2eb2df9904de6c29
Urszula-Molska <105520935+Urszula-Molska@users.noreply.github.com>
2022-10-12 17:59:35 +0000

    Deploying to gh-pages from @ Urszula-Molska/goit-js-hw-09@77abeffc1af153e885d3db94a0df1af25fc192ab 🚀

--- 01-color-switcher.2f7c4519.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(()=>{timerId=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(timerId),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.2f7c4519.js.map
