95b9bf7479a67de3e112fc3aa478ca29f0cfed28
Urszula Molska <urszula_molska@wp.pl>
2022-10-15 17:45:00 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+const form = document.querySelectorAll('.form');
+const button = document.querySelectorAll('button');
+let amount;
+let step;
+let delay;
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const delay = form.elements.delay.value;
+  const step = form.elements.step.value;
+  const amount = form.elements.amount.value;
+  console.log(delay);
+};
+
+form.addEventListener('submit', handleSubmit);
+/* numberOfPromises(amount);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  console.log(shouldResolve);
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve('success');
+      } else {
+        reject('error');
+      }
+    }, delay);
+  });
+  promise
+    .then(({ position, delay }) => {
+      console.log(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`Rejected promise ${position} in ${delay}ms`);
+    });
 }
+
+const numberOfPromises = amount => {
+  for (let i = 0; i < amount; i++) {
+    createPromise(position, delay);
+  }
+};*/
