f78969deff9e36481edd799ed96f3e1d8bb3b677
Urszula Molska <urszula_molska@wp.pl>
2022-10-15 20:16:50 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,21 +1,56 @@
 const form = document.querySelector('.form');
 const button = document.querySelector('button');
-let amount;
-let step;
-let delay;
-let position;
 
-const handleSubmit = event => {
+form.addEventListener('submit', handleSubmit);
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve('success');
+      } else {
+        reject('error');
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let amount = Number(form.elements.amount.value);
+  let step = Number(form.elements.step.value);
+  let timeDelay = Number(form.elements.delay.value);
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, timeDelay)
+      .then(({ position, delay }) => {
+        console.log(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`Rejected promise ${position} in ${delay}ms`);
+      });
+
+    timeDelay = step + timeDelay;
+  }
+}
+
+/*const form = document.querySelector('.form');
+const button = document.querySelector('button');
+
+function handleSubmit(event) {
   event.preventDefault();
   let step = Number(form.elements.step.value);
   let amount = Number(form.elements.amount.value);
   let delay = Number(form.elements.delay.value);
 
   for (let i = 0; i < amount; i++) {
-    createPromise(i, delay);
+    let position = i;
+    createPromise(position, delay);
     delay = step + delay;
   }
-};
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
@@ -37,4 +72,4 @@ function createPromise(position, delay) {
       console.log(`Rejected promise ${position} in ${delay}ms`);
     });
 }
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit);*/
